091017ba2723083f7e9fc41e89e3c3e898ea237d
bencarneiro <bencarneiro@gmail.com>
2024-02-29 17:17:57 -0600

    add db settings

--- austin/austin/settings.py
+++ austin/austin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "app"
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,13 @@ WSGI_APPLICATION = "austin.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "HOST": "localhost",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "maps",
+        "USER": "app",
+        "PASSWORD": "assword",
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
