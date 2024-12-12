a17b621f8e5a2e7ea81f54be1619b6455aad594c
bencarneiro <bencarneiro@gmail.com>
2024-12-12 16:28:05 -0600

    add the shapefiles

--- austin/app/management/commands/delete_all_census_data.py
+++ austin/app/management/commands/delete_all_census_data.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
-from app.models import ACS5ValueByTract
+from app.models import ACS5ValueByTract, ACS1ValueByTract
 
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
+        ACS1ValueByTract.objects.all().delete()
         ACS5ValueByTract.objects.all().delete()
         # print(len(a))
         # for b in a:
